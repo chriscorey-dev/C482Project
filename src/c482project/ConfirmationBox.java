37012274040becaeaf0888af62e00df6872dd98b
Chris Corey <chriscorey.dev@gmail.com>
2019-01-28 12:25:18 -0700

    Done with layouts

--- src/c482project/ConfirmationBox.java
+++ src/c482project/ConfirmationBox.java
@@ -4,7 +4,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
